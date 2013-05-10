ca868becb9a8d1167c9d882c36837fb7fb152b15
gozes <gozes17@gmail.com>
2013-05-09 21:18:40 -0400

    move backup files to tmp and remove tmp on this repo

--- init.el
+++ init.el
@@ -11,3 +11,10 @@
 (add-to-list 'package-archives 
  '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (load-theme 'solarized-light t)
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
