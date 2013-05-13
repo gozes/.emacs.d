1567a7b83c2443e92802d417ebbf82c0408d0f0b
gozes <gozes17@gmail.com>
2013-05-13 19:00:10 -0400

    added zensipder enhance ruby mode neet to play more whit it

---
+++ elpa/enh-ruby-mode-20130507.1726/enh-ruby-mode-autoloads.el
@@ -0,0 +1,18 @@
+;;; enh-ruby-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("enh-ruby-mode-pkg.el" "enh-ruby-mode.el")
+;;;;;;  (20881 24519 370319 880000))
+
+;;;***
+
+(provide 'enh-ruby-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; enh-ruby-mode-autoloads.el ends here
---
+++ elpa/enh-ruby-mode-20130507.1726/enh-ruby-mode-pkg.el
@@ -0,0 +1,2 @@
+(define-package "enh-ruby-mode" "20130507.1726" "Major mode for editing Ruby files [github]" 'nil)
+
