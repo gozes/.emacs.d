e3bc49d9086fd30a07c243f54911f626d61048df
gozes <gozes17@gmail.com>
2013-09-18 17:45:51 -0400

    added termpory org keybindings

--- init.el
+++ init.el
@@ -27,4 +27,9 @@
       `((".*" ,temporary-file-directory t)))
 
 
+;; org-mode keybindes for  learing org MOVE LATER check this http://blog.zenspider.com/blog/2013/06/my-emacs-setup-hooks.html
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
 
