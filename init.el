ed82fe5a9d2ee2bf39dd2e82d99eb647b1756c87
gozes <gozes17@gmail.com>
2013-09-24 17:18:22 -0400

    not sure what i did but lest save it just in case

--- init.el
+++ init.el
@@ -33,3 +33,15 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Code/Projeces/org-mode-test/org.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
