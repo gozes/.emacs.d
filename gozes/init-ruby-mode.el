5efc2715f416f0d0052b168dc6def25cc6f90ea6
gozes <gozes17@gmail.com>
2013-10-20 16:27:06 -0400

    added ruby-end to my ruby config

--- gozes/init-ruby-mode.el
+++ gozes/init-ruby-mode.el
@@ -4,5 +4,6 @@
 (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
+(require 'ruby-end)
 
 (provide 'init-ruby-mode)
