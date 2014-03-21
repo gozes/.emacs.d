1984e4159ecec5e5a13d560729c88d2574d6bca3
gozes <gozes17@gmail.com>
2014-03-21 14:05:07 -0400

    fix small typeo in init for sbcl

--- init.el
+++ init.el
@@ -48,7 +48,7 @@
 
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
   ;; Replace "sbcl" with the path to your implementation
-  (setq inferior-lisp-program "sbcl")
+  (setq inferior-lisp-program "/usr/bin/sbcl")
 
 
-("quicklisp-slime-helper")
+
