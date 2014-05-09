3f805b4b061383ef95c3346ae3812847b9933464
gozes <gozes17@gmail.com>
2014-05-09 17:00:10 -0400

    it makes C-n insert newlines if the point is at the end of the buffer. Useful, as it means you won’t have to reach for the return key to add newlines

--- gozes/init-extentions.el
+++ gozes/init-extentions.el
@@ -5,10 +5,11 @@
 	  (setq ac-auto-show-menu t)
 	  (setq ac-show-menu-immediately-on-auto-complete t)))
 
-
 (req-package melp)
 
 (req-package magit
   :bind ("C-c C-g" . magit-status))
+
+(setq next-line-add-newlines t)
 
 (provide 'init-exten)
