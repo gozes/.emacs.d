8728a71b2cecc94b9ecf50898912e7f1693a4cdb
gozes <gozes17@gmail.com>
2014-05-29 13:35:06 -0400

    switch from racket-mode to geiser

--- gozes/init-racket.el
+++ gozes/init-racket.el
@@ -1,13 +1,27 @@
-(req-package racket-mode
-  :bind ("C-c r" . racket-run)
+(req-package geiser
+  :bind ("C-c r" . run-racket)
   :init (progn
-	  (setq racket-program "/usr/bin/racket")
-	  (setq reco-program "/usr/bin/raco")
-	  (add-hook 'racket-mode-hook 'gozes-racket-mode-guide-key-hook)
-	  (add-hook 'racket-mode-hook 'rainbow-delimiters-mode)))
+	  (add-hook 'geiser-mode-hook 'gozes-geiser-guide-key-hook)))
 
-
-
-(defun gozes-racket-mode-guide-key-hook ()
+(defun gozes-geiser-guide-key-hook ()
   (guide-key/add-local-guide-key-sequence "C-c")
-  (guide-key/add-local-highlight-command-regexp "racket-"))
+  (guide-key/add-local-highlight-command-regexp "geiser-"))
+
+
+
+
+
+
+;; (req-package racket-mode
+;;   :bind ("C-c r" . racket-run)
+;;   :init (progn
+;; 	  (setq racket-program "/usr/bin/racket")
+;; 	  (setq reco-program "/usr/bin/raco")
+;; 	  (add-hook 'racket-mode-hook 'gozes-racket-mode-guide-key-hook)
+;; 	  (add-hook 'racket-mode-hook 'rainbow-delimiters-mode)))
+
+
+
+;; (defun gozes-racket-mode-guide-key-hook ()
+;;   (guide-key/add-local-guide-key-sequence "C-c")
+;;   (guide-key/add-local-highlight-command-regexp "racket-"))
