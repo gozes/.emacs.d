4dfb93aaad72416a312e1f8fea79af4310ab8e41
gozes <gozes17@gmail.com>
2014-08-09 15:25:20 -0400

    added config for elpy/python mode

---
+++ gozes/init-python.el
@@ -0,0 +1,12 @@
+(req-package elpy
+  :init (progn
+	  (add-hook 'elpy-mode-hook 'gozes-elpy-mode-guide-key-hook)
+	  (custom-set-variables
+	   '(elpy-rpc-python-command "python3")
+	   '(elpy-rpc-backend "jedi"))))
+
+
+(defun gozes-elpy-mode-guide-key-hook ()
+  (guide-key/add-local-guide-key-sequence "C-c")
+  (guide-key/add-local-guide-key-sequence "M-")
+  (guide-key/add-local-highlight-command-regexp "elpy-"))
