ced49a34c72e042df2a30363c1529240073ab3f9
gozes <gozes17@gmail.com>
2014-09-07 18:42:18 -0400

    stupid merge

--- init.el
+++ init.el
@@ -18,19 +18,7 @@
 ;; update the buffuer if a file has change on disk
 (global-auto-revert-mode)
 
-<<<<<<< HEAD
 (add-hook 'after-init-hook #'(lambda () (load (concat user-emacs-directory "gozes-bootstrap.el"))))
-=======
-(eval-after-load 'org-mode (require 'init-org-mode))
-(eval-after-load 'emacs-lisp-mode (require 'init-elisp))
-(add-hook 'ruby-mode-hook '(lambda () (require 'init-ruby-mode)))
-(eval-after-load 'clojure-mode '(require 'init-clojure-mode))
-;; you need to symlink your erc config to ercrc
-;; ln -s ~/.emacs.d/gozes/init-erc.el  ~/.emacs.d/.ercrc.el
-
-
-
->>>>>>> cecaae95aaac9f9622daff599dcd8fffdd3913eb
 
 (elpy-enable)
 
