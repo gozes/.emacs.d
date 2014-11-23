1bc3a06cfe71bb63b9b59532cd9b896c15b88110
gozes <gozes17@gmail.com>
2014-11-23 16:23:30 -0400

    hide nrepl conncet and server buffer from swithc to buffer

--- gozes/init-clojure-mode.el
+++ gozes/init-clojure-mode.el
@@ -4,6 +4,7 @@
     (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
     (setq cider-repl-pop-to-buffer-on-connect nil)
     (setq cider-show-error-buffer nil)
+    (setq nrepl-hide-special-buffers t)
     (setq cider-repl-display-in-current-window t)
     (setq cider-repl-result-prefix ";; => ")
     (add-hook 'cider-repl-mode-hook 'smartparens-strict-mode)
