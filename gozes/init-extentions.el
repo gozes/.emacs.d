f054ba0d75634826954fb3be307cc95fd15d15ec
gozes <gozes17@gmail.com>
2014-12-03 14:09:42 -0400

    added show smartparens highlight match to my config

--- gozes/init-extentions.el
+++ gozes/init-extentions.el
@@ -58,7 +58,8 @@
   :init
   (progn
     (require 'smartparens-config)
-    (smartparens-global-mode t)))
+    (smartparens-global-mode t)
+    (show-smartparens-global-mode t)))
 
 
 (provide 'init-exten)
