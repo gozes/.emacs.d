f34dc1b7094f4905b499bdbea3ec0426237ad94b
gozes <gozes17@gmail.com>
2014-12-14 19:06:29 -0400

    set go-mode to use goimports insted of gofmt

--- gozes/init-go.el
+++ gozes/init-go.el
@@ -1,6 +1,7 @@
 (req-package go-mode
   :config (progn
-	    (add-hook 'go-mode-hook 'go-eldoc-setup)))
+	    (add-hook 'go-mode-hook 'go-eldoc-setup)
+	    (setq gofmt-command "goimports")))
 
 (req-package go-eldoc)
 (req-package company-go)
