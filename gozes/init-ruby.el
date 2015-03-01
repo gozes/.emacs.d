132b4b4d415549fce4b4e23c3d258b2f6bc0eaf7
gozes <gozes17@gmail.com>
2015-03-01 18:01:32 -0400

    more defer

--- gozes/init-ruby.el
+++ gozes/init-ruby.el
@@ -1,4 +1,5 @@
 (req-package robe
+  :defer t
   :init
   (progn
     (add-hook 'ruby-mode-hook 'robe-mode)
@@ -9,6 +10,7 @@
     (push 'company-robe company-backends)))
 
 (req-package minitest
+  :defer t 
   :init
   (progn
     (add-hook 'ruby-mode-hook 'minitest-mode)))
