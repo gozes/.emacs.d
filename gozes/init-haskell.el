f27b9a15076ef3d0967575bb29fe4efb96c9a346
gozes <gozes17@gmail.com>
2015-03-19 14:40:06 -0400

    redo of my haskell setup

--- gozes/init-haskell.el
+++ gozes/init-haskell.el
@@ -1,21 +1,24 @@
-;; (req-package haskell-mode
-;;   :init (progn
-;; 	   (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-;; 	   (add-hook 'haskell-mode-hook 'flycheck-mode)
-;; 	   (add-hook 'haskell-mode-hook 'gozes-haskell-mode-guide-key-hook)
-;; 	   (custom-set-variables
-;; 	    '(haskell-process-suggest-remove-import-lines t)
-;; 	    '(haskell-process-auto-import-loaded-modules t)
-;; 	    '(haskell-process-log))))
+(req-package haskell-mode
+  :defer t
+  :config
+  (progn
+    (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+    (add-to-list 'company-backends 'company-ghc)
+    (add-hook 'haskell-mode-hook 'structured-haskell-mode)))
+
+(req-package shm
+  :defer t)
+
+(req-package company-ghc
+  :defer t)
+
+(req-package ghc
+  :defer t)
 
 
-;; (defun gozes-haskell-mode-guide-key-hook ()
-;;   (guide-key/add-local-guide-key-sequence "C-")
-;;   (guide-key/add-local-highlight-command-regexp "haskell-"))
 
-;; (bind-key "C-," 'haskell-move-nested-left haskell-mode-map)
-;; (bind-key "C-." 'haskell-move-nested-right haskell-mode-map)
-;; (bind-key "C-x a r" 'align-regexp)
+
+
 
 
 
--- gozes/init-python.el
+++ gozes/init-python.el
@@ -1,25 +1,25 @@
-(req-package anaconda-mode
-  :defer t
-  :init
-  (progn
-    (add-hook 'python-mode-hook 'anaconda-mode)
-    (setq python-shell-interpreter "python2.7"))
-  :config
-  (progn
-    (setq python-shell-virtualenv-path nil)
-    (bind-key "C-c C-c r" 'gozes-anaconda-mode-python3 anaconda-mode)
-    (bind-key "C-c C-c g" 'anaconda-mode-goto-definitions anaconda-mode)
-    (bind-key "C-c C-c p" 'anaconda-nav-pop-marker anaconda-mode)
-    (defun gozes-anaconda-mode-python3 ()
-      (interactive)
-      (setq python-shell-interpreter "python3.4")
-      (anaconda-mode-start))))
+;; (req-package anaconda-mode
+;;   :defer t
+;;   :init
+;;   (progn
+;;     (add-hook 'python-mode-hook 'anaconda-mode)
+;;     (setq python-shell-interpreter "python2.7"))
+;;   :config
+;;   (progn
+;;     (setq python-shell-virtualenv-path nil)
+;;     (bind-key "C-c C-c r" 'gozes-anaconda-mode-python3 anaconda-mode)
+;;     (bind-key "C-c C-c g" 'anaconda-mode-goto-definitions anaconda-mode)
+;;     (bind-key "C-c C-c p" 'anaconda-nav-pop-marker anaconda-mode)
+;;     (defun gozes-anaconda-mode-python3 ()
+;;       (interactive)
+;;       (setq python-shell-interpreter "python3.4")
+;;       (anaconda-mode-start))))
 
-(req-package company-anaconda
-  :defer t
-  :init
-  (progn
-    (add-to-list 'company-backends 'company-anaconda)))
+;; (req-package company-anaconda
+;;   :defer t
+;;   :init
+;;   (progn
+;;     (add-to-list 'company-backends 'company-anaconda)))
 
 
 
