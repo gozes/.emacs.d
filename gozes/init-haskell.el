f73f7dfdbd8d63d4ae773142de3b6cac7f85304a
gozes <gozes17@gmail.com>
2015-04-03 11:23:42 -0400

    more haskell setup

--- gozes/init-haskell.el
+++ gozes/init-haskell.el
@@ -4,7 +4,12 @@
   (progn
     (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
     (add-to-list 'company-backends 'company-ghc)
-    (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)))
+    (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+    (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+    (custom-set-variables
+     '(haskell-process-suggest-remove-import-lines t)
+     '(haskell-process-auto-import-loaded-modules t)
+     '(haskell-process-log t))))
 
 ;; (req-package shm
 ;;   :defer t)
