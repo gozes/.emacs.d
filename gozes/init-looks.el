60fad6c16652ae439c8c6b89f433c813f151e9eb
gozes <gozes17@gmail.com>
2015-04-09 10:05:31 -0400

    changed theme to moe dark

--- gozes/init-looks.el
+++ gozes/init-looks.el
@@ -1,11 +1,11 @@
 ;; (req-package phoenix-dark-pink-theme
 ;;   :init (load-theme 'phoenix-dark-pink t))
 
-(req-package cyberpunk-theme
-  :init (load-theme 'cyberpunk t))
+;; (req-package cyberpunk-theme
+;;   :init (load-theme 'cyberpunk t))
 
-;; (req-package moe-theme
-;;   :init (load-theme 'moe-dark t))
+(req-package moe-theme
+  :init (load-theme 'moe-dark t))
 
 ;; (req-package sublime-themes
 ;;   :init (load-theme 'brin t))    
