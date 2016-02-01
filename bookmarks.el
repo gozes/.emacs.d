762ec18abb776a6187b022e3eb18c06bcf2f314c
gozes <gozes17@gmail.com>
2016-02-01 12:43:59 -0400

    filenaly got bookmarks working the way I want.

--- bookmarks.el
+++ bookmarks.el
@@ -1,1 +1,10 @@
-; just so git will save it
+;;;; Emacs Bookmark Format Version 1 ;;;;
+;;; This format is meant to be slightly human-readable;
+;;; nevertheless, you probably don't want to edit it.
+;;; -*- End Of Bookmark File Format Version Stamp -*-
+(("config.org"
+ (filename . "~/.emacs.d/config.org")
+ (front-context-string . "nd Bookmarks\n** ")
+ (rear-context-string)
+ (position . 12))
+)
