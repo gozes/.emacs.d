b5eae7bfaa885746d4995b8f9025e82a7d1ee57d
gozes <gozes17@gmail.com>
2016-04-11 14:53:13 -0400

    added fish mode to my config

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bookmark-default-file "~/.emacs.d/bookmarks.el"))
+ '(bookmark-default-file "~emacs.d/bookmarks.el"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
