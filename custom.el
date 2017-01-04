cf6efe1588c0614ed35e753be69b5d602b04b09b
gozes <gozes17@gmail.com>
2017-01-04 10:13:26 -0400

    initial typescript configuration

--- custom.el
+++ custom.el
@@ -3,7 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bookmark-default-file "~emacs.d/bookmarks.el"))
+ '(bookmark-default-file "~emacs.d/bookmarks.el")
+ '(package-selected-packages
+   (quote
+    (json-mode fish-mode flycheck exec-path-from-shell restart-emacs discover-my-major markdown-mode smartparens helm-descbinds helm magit company-go go-eldoc go-mode robe elpy alchemist cyberpunk-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
